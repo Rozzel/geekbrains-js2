eaf18462e76426f41306d6a0ec561ed1cd4d3efe
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-02-04 00:46:27 +0300

    data.length

--- lessons-2/3-dz/scripts/script.js
+++ lessons-2/3-dz/scripts/script.js
@@ -7,7 +7,7 @@ let fullPhoto = document.getElementById('gallery__photo-full');
         
         fullPhoto.innerHTML += `<img class="full-photo" src="${data[0].srcl}" alt="${data[0].alt}">`;
 
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < data.length; i++) {
             photoPreviews.innerHTML += `<button class="gallery__photo-preview" type="button"><img src="${data[i].srcs}" alt="${data[i].alt}"></button>`;
         }
       })
