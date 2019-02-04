c8445e7bed0f628275a6de3e40b11814601ef170
Vladislav <rozzel@mail.ru>
2019-02-04 15:36:13 +0300

    dz1-2-3

--- lessons-2/1-2-3-dz/scripts/menu.render.js
+++ lessons-2/1-2-3-dz/scripts/menu.render.js
@@ -6,20 +6,23 @@ class Menu {
     }
     render(){
         let result = `<ul class="${this.className}" id="${this.id}">`;
-
         for (let item of this.items){
             if (item instanceof MenuItem || item instanceof SubMenu){
                 result += item.render();
-            } else if (item instanceof SubMenu){
-                result += item.render();
             }
         }
-
         result += `</ul>`;
         return result;
     }
 }
 
-class SubMenu extends Menu {
-    constructor(href, title, id, className, items)
+class SubMenu extends Menu{
+    constructor(href, title, id, className, items){
+        super(id, className, items);
+        this.href = href;
+        this.title = title;
+    }
+    render(){
+        return `<li><a href="${this.href}">${this.title}</a>${super.render()}</li>`
+    }
 }
