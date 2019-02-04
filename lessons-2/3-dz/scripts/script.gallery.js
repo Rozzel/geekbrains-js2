34e2cc654e98a4670cb3ad0c23bdfe0fa45404d2
Vladislav <rozzel@mail.ru>
2019-02-04 11:23:00 +0300

    Провки dz3
    Мелкие правки

--- lessons-2/3-dz/scripts/script.gallery.js
+++ lessons-2/3-dz/scripts/script.gallery.js
@@ -5,7 +5,7 @@ let fullImg = document.getElementById('gallery__photo-full');
       .then(result => result.json())
       .then(data => {
         
-        fullImg.innerHTML += `<img class="full-photo" src="${data[0].srcl}" alt="${data[0].alt}">`;
+        fullImg.innerHTML = `<img class="full-photo" src="${data[0].srcl}" alt="${data[0].alt}">`;
 
         for (let i = 0; i < data.length; i++) {
             photoPreviews.innerHTML += `<button class="gallery__photo-preview" type="button"><img src="${data[i].srcs}" alt="${data[i].alt}"></button>`;
@@ -14,9 +14,9 @@ let fullImg = document.getElementById('gallery__photo-full');
         let thumbnails = document.querySelectorAll('.gallery__photo-preview');
         let fullPhoto = document.querySelector('.full-photo');
         
-        let addThumbnailClickHandler = function (thumbnail, photo, alt) {
+        let addThumbnailClickHandler = function (thumbnail, src, alt) {
           thumbnail.addEventListener('click', function () {
-            fullPhoto.src = photo;
+            fullPhoto.src = src;
             fullPhoto.alt = alt;
           });
         };
